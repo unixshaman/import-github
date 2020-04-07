9a1b502c59983833533d9d0be6b958b83e3fc679
maxinfosoft <maxinfosoft@gmail.com>
2020-04-07 22:35:56 +0300

    Add parameters

--- import-github.groovy
+++ import-github.groovy
@@ -1,4 +1,8 @@
 pipeline {
+  parameters {
+    string(defaultValue: 'asdf', description: 'asfd asdf', name: 'asdf1', trim: true)
+    string(defaultValue: 'asdf', description: 'asfd asdf', name: 'asdf2', trim: true)
+  }
   agent {
     node {
        label 'slave'
